0778e944b91a3d221a53d393f9616398ce624de1
levell1 <whddnr0317@naver.com>
2022-01-30 22:00:04 +0900

    update

--- Triggerevent.cs
+++ Triggerevent.cs
@@ -9,17 +9,21 @@ public class Triggerevent : MonoBehaviour
     [SerializeField]
     private Vector3     moveDirection;
     private float       movespeed;
+    private SpriteRenderer spriteRenderer;
     private void Awake() {
         movespeed = 5.0f;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
     private void OnTriggerEnter2D(Collider2D collision) {
         moveObject.GetComponent<SpriteRenderer>().color = Color.black;
+        spriteRenderer.color = Color.gray;
     }
     private void OnTriggerStay2D(Collider2D collision) {
         moveObject.transform.position += moveDirection * movespeed*Time.deltaTime;
     }
     private void OnTriggerExit2D(Collider2D collision) {
         moveObject.GetComponent<SpriteRenderer>().color =Color.white;
+        spriteRenderer.color = Color.white;
         //moveObject.transform.position = new Vector3(0,4,0);
     }
 }
